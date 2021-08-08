c86bd2bfbb5b9b6905442ad71600c1cbe9a067db
Zekao <Busterk182@gmail.com>
2021-08-08 15:57:24 +0200

    Fixin' substr

--- srcs/ft_substr.c
+++ srcs/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: emaugale <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 22:54:23 by emaugale          #+#    #+#             */
-/*   Updated: 2021/08/05 13:45:35 by emaugale         ###   ########.fr       */
+/*   Updated: 2021/08/08 15:56:37 by emaugale         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
 	char	*newstr;
-	size_t	size;
 
-	size = len - start;
-	if (s == 0)
-		return (NULL);
-	newstr = malloc(sizeof(char) * (size) + 1);
+	newstr = malloc(sizeof(char) * (len + 1));
 	if (!(newstr))
 		return (0);
 	i = 0;
-	while (i < size)
-		newstr[i++] = s[start++];
+	while (i < len)
+	{
+		newstr[i] = s[start + i];
+		i++;
+	}
 	newstr[i] = '\0';
 	return (newstr);
 }
